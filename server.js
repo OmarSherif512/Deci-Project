83a6d2205a40fb566dc853b9519b6cfdbb5ba1b8
OmarSherif512 <omarvalo46@gmail.com>
2026-09-17 20:14:14 +0300

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static("public"));
 
 const bookings = [];
 
